9b24d9ca938eb79b667320c2928b4673c78014bb
shenn0n <shenn0n4412@gmail.com>
2024-09-03 09:55:40 +0300

    Initial Commit

---
+++ Personality Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Александр Манжосов on 03.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
